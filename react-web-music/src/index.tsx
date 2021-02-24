ddd7471b724238e9d15b54d07a6af8528d314be5
Pokkio <1198659788@qq.com>
2021-02-24 22:56:20 +0800

    refactor: remove react-web-music

--- react-web-music/src/index.tsx
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-import '@/assets/css/reset.css'
-
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
